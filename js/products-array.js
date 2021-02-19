6709cffb3a4149762d1816a755763774087c83a5
AndresSalomon1990 <a.salomon.1990@gmail.com>
2021-02-18 21:03:32 -0300

    Add products array with imgs

---
+++ js/products-array.js
@@ -0,0 +1,30 @@
+const products = [
+    {
+        id: 1,
+        modelo: "HP Dk-1003",
+        marca: "HP",
+        precio: 500,
+        img: "../assets/hp.jpg"
+    },
+    {
+        id: 2,
+        modelo: "Dell Inspiron 5593",
+        marca: "Dell",
+        precio: 780,
+        img: "../assets/dell.jpg"
+    },
+    {
+        id: 3,
+        modelo: "Asus Ultrabook Gamer",
+        marca: "Asus",
+        precio: 900,
+        img: "../assets/asus.jpg"
+    },
+    {
+        id: 4,
+        modelo: "McBook Air",
+        marca: "Apple",
+        precio: 1200,
+        img: "../assets/mcbook.jpg"
+    }
+]
